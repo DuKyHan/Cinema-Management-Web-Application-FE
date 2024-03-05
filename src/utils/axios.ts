fb2885390ad35e29f60dab09c5e9e25486d20f4c
DuKyHan <dukyhan2312@gmail.com>
2024-03-05 21:16:07 +0700

    changing axios gate

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//export const BASE_URL = 'http://128.199.251.131:3000/api/v1';
-export const BASE_URL = 'http://localhost:3000/api/v1';
+export const BASE_URL = 'http://128.199.251.131:3000/api/v1';
+// export const BASE_URL = 'http://localhost:3000/api/v1';
 
 export const rawAxiosInstance = axios.create({
   baseURL: BASE_URL,
